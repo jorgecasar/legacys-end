5de77644f203d169f3a35b38e83c44c9f55d00f5
Jorge del Casar <948953+jorgecasar@users.noreply.github.com>
2025-12-30 19:45:43 +0100

    feat: refactor event handlers and fix types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="vite/client" />
+/// <reference types="dom-speech-recognition" />
 
 // Extend ImportMetaEnv with custom environment variables if needed
 // interface ImportMetaEnv {
